b6762ab0454cae619cb8ae67d4612399e436e950
tawanda <tawanda.nyakudjga@gmail.com>
2016-12-18 09:43:40 +0800

    add title to modal

--- public/crm/src/util/modal.vue
+++ public/crm/src/util/modal.vue
@@ -4,15 +4,14 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title" id=""></h4>
+            <h4 class="modal-title" >{{title}}</h4>
           </div>
           <div class="modal-body">
               <slot></slot>
           </div>
           <div class="modal-footer">
-             <slot>
-                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                 <button type="button" class="btn btn-primary"></button>
+             <slot name="footer">
+                 <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
              </slot>
           </div>
         </div>
@@ -29,6 +28,9 @@ export default {
         },
         show:{
             default:false
+        },
+        title:{
+            default:""
         }
     },
     data:function () {
